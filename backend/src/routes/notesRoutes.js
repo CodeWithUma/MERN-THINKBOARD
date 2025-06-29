0c7d36421e1773a9c6e4f55586e42b829cbad0fd
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-06-29 13:57:49 +0530

    5- Completing Our Controllers

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,7 +1,7 @@
 // This file is created to separate the routes for notes from the main server file
 // and set up the basic structure for handling notes-related requests.
 import express from 'express';
-import { getAllNotes, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
+import { getAllNotes, getNoteById, createNote, updateNote, deleteNote } from '../controllers/notesController.js';
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ const router = express.Router();
 // that lets the client/user interact with the specific resource.
 
 router.get('/', getAllNotes);
+router.get("/:id", getNoteById);
 router.post('/', createNote);
 router.put('/:id', updateNote);
 router.delete('/:id', deleteNote);
