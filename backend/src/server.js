7ec2a672af170a1afc8a4c6ac148965a2572cb37
Uma Prasad Das <umaprasaddas1999@gmail.com>
2025-06-29 11:02:04 +0530

    4- Setting Up Our Database (MongoDB)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+// Connect to the database
+// This function will connect to the MongoDB database using Mongoose.
+connectDB();
 
 app.use(express.json());
 // Whenever we want to use a specific route, we can use the app.use method
 // and specify the base path for that route.
 app.use('/api/notes', notesRoutes);
 
-app.listen(5001, () => {
-    console.log('Server is running on port 5001');
+app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
 });
